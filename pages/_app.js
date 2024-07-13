736ce9fa6f54cbb27c14439a71ed761b3b9f17b6
Som S. <somwrks@gmail.com>
2024-07-13 15:11:06 +0530

    e

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [usertype, setUsertype] = useState("");
+
+  useEffect(() => {
+    const storedUsertype = localStorage.getItem("usertype");
+    if (storedUsertype) {
+      setUsertype(storedUsertype);
+    }
+  }, []);
+
+  const handleUserTypeChange = (newUserType) => {
+    setUsertype(newUserType);
+    localStorage.setItem("usertype", newUserType);
+  };
+
+  return (
+    <ClerkProvider>
+      <Component
+        setUsertype={handleUserTypeChange}
+        usertype={usertype}
+        {...pageProps}
+      />
+    </ClerkProvider>
+  );
 }
